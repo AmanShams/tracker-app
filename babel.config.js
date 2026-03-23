9dd64c19d34ac9853eda07fade2677c417a00e99
DESKTOP-59BD7I3\aman <amanullahshamsi22@gmail.com>
2026-03-23 14:35:26 +0500

    AM : fix error

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      'babel-plugin-transform-import-meta'
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformImportMeta: true,
+        },
+      ],
     ],
+    plugins: ['babel-plugin-transform-import-meta'],
   };
 };
